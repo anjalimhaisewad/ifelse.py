139e0ea099cb6d04b195032395a2ef0cb29e9c03
anjalimhaisewad <113433513+anjalimhaisewad@users.noreply.github.com>
2022-12-13 14:33:18 +0530

    Add files via upload

---
+++ positive to negative.py
@@ -0,0 +1,16 @@
+# a=int(input("enter a no"))
+# if a>0:
+#     print(a*-1)
+# elif a<0:
+#     print(a*-1)
+# else:
+#     print(0)
+
+
+# a=int(input("enter a no"))
+# if a>0:
+#     print(-a)
+# elif a<0:
+#     print(-a)
+# else:
+#     print(0)
